bc4faa648c9af801768ae8da854c5b8d25d9a655
haowenchao <haowenchao123@gmail.com>
2019-10-11 14:38:13 +0800

    go basic1

---
+++ src/ch1/echo1.go
@@ -0,0 +1,18 @@
+//Page 3
+//echo1 print the arguments in command line
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
---
+++ src/ch1/echo4.go
@@ -0,0 +1,13 @@
+// Page 6 Practive 1.1 & 1.2
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println(os.Args[0])
+
+}
